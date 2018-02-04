1f30d4e9116475257ed058fcd68b4b95dbac439a
Shloak Jain <shloak@gmail.com>
2018-02-03 17:04:54 -0800

    chainging height of layers

--- model.py
+++ model.py
@@ -19,8 +19,8 @@ def conv_out_size_same(size, stride):
 class DCGAN(object):
   def __init__(self, sess, input_height=256, input_width=320, crop=False,
          batch_size=64, sample_num = 64, output_height=256, output_width=320,
-         y_dim=None, z_dim=2000, gf_dim=64, df_dim=64, # change z_dim * 20
-         gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='train_img_slices', 
+         y_dim=None, z_dim=2000, gf_dim=640, df_dim=640, # change z_dim * 20
+         gfc_dim=10240, dfc_dim=10240, c_dim=3, dataset_name='train_img_slices', 
          input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None):
     """
 
