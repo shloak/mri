f9ab659fadaeabb42c3edc29b9695c17542c2220
Shloak Jain <shloak@gmail.com>
2018-02-05 14:58:30 -0800

    changed z

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ def conv_out_size_same(size, stride):
 class DCGAN(object):
   def __init__(self, sess, input_height=64, input_width=64, crop=False,
          batch_size=2, sample_num = 64, output_height=64, output_width=64,
-         y_dim=None, z_dim=2000, gf_dim=64, df_dim=64, # change z_dim * 20
+         y_dim=None, z_dim=100, gf_dim=64, df_dim=64, # change z_dim * 20
          gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='train_img_slices', 
          input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None):
     """
